e86270c95a61c2244d78da43e8b8f7f8ce815b36
diaz <jerylfranz460@gmail.com>
2022-11-04 19:03:23 +0400

    takes right instead of left

--- app.js
+++ app.js
@@ -24,40 +24,50 @@
 //   return c
 // }
 
-function divide(a,b,arr){
-  let c =[]
-
-  // return c if a is empty
-  for(; a< b;a++){
+let c = []
+function divide(a, b, arr) {
+  for (; a < b; a++) {
     c.push(arr[a])
   }
-  // for(let i = 0;i < c.length ; i++){
-  //   arr.shift()
-  //   }
-  return c
-}
-
-let arr = [41,10,5,6,45,2,14,0]
-console.log(arr)
-
-let l = 0;
-let r = arr.length 
-let m =  Math.round((l+r)/2);
-
-let newArr = divide(l,m,arr)
-console.log(newArr)
-
-let newR = newArr.length
-let newM = Math.round((l+newR)/2)
-
-let newNewArr = divide(l,newM,newArr)
-
-console.log(newNewArr)
-
-function mergeSort(arr){
-  let l = 0;
-  let r = arr.length;
-  if(l<r){
-    let m =  Math.round((l+r)/2);
+  for (let i = 0; i < c.length; i++) {
+    arr.shift()
+  }
+  if (c.length > 1) {
+    let l = 0;
+    let r = c.length
+    let m = Math.round((l + r) / 2);
+    return divide(a, m, c)
+  } else {
+    console.log(c)
+    return c
   }
 }
+
+let arr = [41, 10, 5, 6]
+// // console.log(arr)
+
+let l = 0;
+let r = arr.length
+let m = Math.round((l + r) / 2);
+
+let newArr = divide(l, m, arr)
+// console.log(newArr)
+
+// // let newR = newArr.length
+// // let newM = Math.round((l+newR)/2)
+
+// // let newNewArr = divide(l,newM,newArr)
+// // console.log(newNewArr)
+
+
+// function mergeSort(arr) {
+//   let l = 0;
+//   let r = arr.length;
+//   if (l < r) {
+//     let m = Math.round((l + r) / 2);
+//   }
+// }
+//
+//
+//
+
